4eaefe7d588152775d6baf64731b7875d658fa6f
Iñigo Lara <ila@Inilariz.local>
2022-06-09 10:11:13 +0200

    restructuration of error-catching

--- src/CLIParser.py
+++ src/CLIParser.py
@@ -1,15 +1,10 @@
 import argparse
 import os
-from DataConverter import DataConverter
 
 class Parser:
 
-    MAX_ADDR = 32767  # 15 bit address register
-
-
     def __init__(self):
         self.parser = self.create_parser()
-        self.converter = DataConverter()
 
 
     def create_parser(self):
@@ -43,45 +38,4 @@ class Parser:
 
     def msg(name=None):                                                            
         return '''nuensis [-h] '''
-
-
-    def valid_address(self, addr_hex_str):
-        try:
-            addr_int = self.converter._hex_str_to_int(addr_hex_str)
-            if addr_int <= self.MAX_ADDR and addr_int >= 0:
-                return True
-            else:
-                print("Wrong Address:")
-                print("Address must be positive and 15-bit long at maximum (smaller than 0x7fff):")
-        except:
-            print("Wrong Address:")
-            
-        return False
-
-
-    def valid_value(self, value_hex_str):
-        try:
-            value_int = self.converter._hex_str_to_int(value_hex_str)
-            if value_int <= 255 and value_int >= 0:
-                return True
-            else:
-                print("Wrong Value:")
-                print("Value must be a positive 8-bit HEX number (smaller than 0xff):")
-        except:
-            print("Wrong Value:")
-
-        return False
-
-
-    def valid_arguments(self, args):
-        if args.command == "read":
-            if args.address:
-                return self.valid_address(args.address[0])
-            elif args.range:
-                return self.valid_address(args.range[0]) and self.valid_address(args.range[1])
-        elif args.command == "write":
-            if args.address:
-                return self.valid_value(args.address[1]) and  self.valid_address(args.address[0])
-            if args.whole:
-                return self.valid_value(args.whole[0])
     
--- src/main_amanuensis.py
+++ src/main_amanuensis.py
@@ -18,14 +18,14 @@ if __name__ == "__main__":
 #            else:
 #                print("Bad usage. Pass no parameters")
         elif args.address:
-            if parser.valid_arguments(args):
+            if eeprom.valid_arguments(args):
                 addr_str = args.address[0]
                 print(f"READING ADDRESS 0x{addr_str}:\n")
                 eeprom.read_routine('single', addr_str)
             else:
                 print("Try [-a A], where A is a valid HEX Address (e.g. 10ef)")
         elif args.range:
-            if parser.valid_arguments(args):
+            if eeprom.valid_arguments(args):
                 begin_str = args.range[0]
                 end_str = args.range[1]
                 print(f"READING RANGE 0x{begin_str} TO 0x{end_str}:\n")
@@ -34,21 +34,21 @@ if __name__ == "__main__":
                 print("Try [-r A1 A2], where A_ is a valid HEX Address (e.g. 10ef)")
     elif args.command == 'write':
         if args.address:
-            if parser.valid_arguments(args):
+            if eeprom.valid_arguments(args):
                 addr_str = args.address[0]
                 value_str = args.address[1]
                 print(f"WRITING ADDRESS 0x{addr_str} WITH VALUE 0x{value_str}:")
                 eeprom.write_routine('value', addr_str, value_str)
             else:
                 print("Try [-v A V] where A is a valid HEX Address (e.g. 10ef)")
-                print("and V(alue) a 1-byte HEX value (e.g. ab)")
+                print("...and where V is a valid HEX value (e.g. ab)")
         if args.file:
             print("FILE WRITTING:")
             filepath = args.file[0]
             working_directory = args.wd
             eeprom.write_routine('file', filepath, working_directory)
         if args.whole:
-            if parser.valid_arguments(args):
+            if eeprom.valid_arguments(args):
                 value = args.whole[0]
                 print(f"OVERWRITTING WHOLE MEMORY WITH VALUE 0x{value}:")
                 eeprom.write_routine('all', value)
